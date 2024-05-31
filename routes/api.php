3a6056eadfcbb73fd45cde20fad463125899d269
Christian <kristian2015ok@gmail.com>
2024-05-31 14:32:34 +0700

    Add: Pagination

--- routes/api.php
+++ routes/api.php
@@ -45,3 +45,8 @@ Route::get('/products', function () {
     $products = Product::all();
     return new ProductCollection($products);
 });
+Route::get('/products-paging', function (Request $request) {
+    $page = $request->get('page', 1);
+    $products = Product::paginate(perPage: 2, page: $page);
+    return new ProductCollection($products);
+});
